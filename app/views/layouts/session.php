51ff954dc66148091a895bd60b39f61aee5f5822
JOHANDUARTE35 <johanduarte35@gmail.com>
2023-03-15 20:59:27 -0500

    nice

--- app/views/layouts/session.php
+++ app/views/layouts/session.php
@@ -1,7 +1,8 @@
 <?php
 // Initialize the session
-    // session_start();
-
+if(!isset($_SESSION)){
+    session_start();
+}
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["unique_id"])){
     header("location: login");
